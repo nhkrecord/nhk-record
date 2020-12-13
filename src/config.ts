bc69997640858ee1aa65290641dc77a65cabfadd
nhkrecord <74484500+nhkrecord@users.noreply.github.com>
2020-12-12 22:31:55 -0800

    feat: expose option defaults on command line (#14)
    implements #12

--- src/config.ts
+++ src/config.ts
@@ -18,12 +18,14 @@ const config = yargs(process.argv.slice(2))
   .option('a', {
     alias: 'assets-url',
     describe: 'NHK assets url (for JS & thumbnails)',
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.assetsUrl
   })
   .option('b', {
     alias: 'safety-buffer',
     describe: 'Number of extra milliseconds to record before and after scheduled airtime',
-    type: 'number'
+    type: 'number',
+    default: defaultConfig.safetyBuffer
   })
   .option('c', {
     alias: 'config',
@@ -34,46 +36,54 @@ const config = yargs(process.argv.slice(2))
   .option('d', {
     alias: 'save-dir',
     describe: 'Directory in which to save recorded programmes',
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.saveDir
   })
   .option('f', {
     alias: 'log-file',
     describe: 'Location of log file',
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.logFile
   })
   .option('i', {
     alias: 'stream-url',
     describe: 'URL from which to record stream',
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.streamUrl
   })
   .option('k', {
     alias: 'log-level-console',
     describe: 'Logging level to output to console',
     choices: ['debug', 'info', 'error', 'none'],
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.logLevelConsole
   })
   .option('l', {
     alias: 'log-level-file',
     describe: 'Logging level to output to log file',
     choices: ['debug', 'info', 'error', 'none'],
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.logLevelFile
   })
   .option('m', {
     alias: 'match-pattern',
     describe: 'Glob pattern of desired program name (can be used multiple times)',
     type: 'string',
-    array: true
+    array: true,
+    default: defaultConfig.matchPattern
   })
   .option('s', {
     alias: 'schedule-url',
     describe: 'NHK schedule API url',
-    type: 'string'
+    type: 'string',
+    default: defaultConfig.scheduleUrl
   })
   .option('t', {
     alias: 'minimum-duration',
     describe: 'Minimum programme run time to record in milliseconds',
-    type: 'number'
+    type: 'number',
+    default: defaultConfig.minimumDuration
   }).argv;
 
 //TODO: validate config
-export default ({ ...defaultConfig, ...config } as unknown) as Config;
+export default (config as unknown) as Config;
